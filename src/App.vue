8b8e419363a918b58d0de4ba1f5d8114b5e80edf
zdp <suol0920@163.com>
2021-12-02 08:43:33 +0800

    引入element-ui并处理公共样式

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <template>
   <div id="app">
     <h1>哈利波特</h1>
+    <p class="test">test</p>
     <!-- 根路由出口 -->
     <router-view/>
   </div>
 </template>
+<script lang="ts">
+import Vue from 'vue'
 
-<style lang="scss" scoped></style>
+export default Vue.extend({
+  name: 'App'
+})
+</script>
+
+<style lang="scss" scoped>
+.test {
+  color: $danger-color;
+}
+</style>
